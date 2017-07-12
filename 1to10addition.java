960ec02e907270414f1d2d76b2a2b91a898a9ba2
prushly <purushothptm@gmail.com>
2017-07-12 11:14:10 +0530

    Add files via upload

---
+++ 1to10addition.java
@@ -0,0 +1,19 @@
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=1;
+		int b=0;
+	while(a<=10){
+		
+	
+		b=a+b;
+		a++;
+	
+		
+	}
+	System.out.println(b);
+	}
+
+}
